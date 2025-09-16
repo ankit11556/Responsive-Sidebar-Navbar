b81abd75385b56973ec430fe80e5c96eb70b4f82
ankit <saharan442090@gmail.com>
2025-09-16 17:05:34 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Sidebar from './components/Sidebar'
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
   
 
   return (
     <>
      <Sidebar></Sidebar>
+     <Navbar></Navbar>
     </>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 const Sidebar = ()=>{
   return(
-   <aside className="hidden h-screen w-64 lg:flex bg-gray-600 text-white fixed flex-col items-start gap-4 p-6 ">
-      <h2 className="text-2xl">My App</h2>
+   <aside className="hidden h-screen w-64 lg:flex bg-gray-600 text-white fixed flex-col   ">
+      <h2 className="text-2xl p-4">My App</h2>
       
-   <ul className="flex flex-col gap-2 text-lg  mt-4 w-full">
+   <ul className="mt-4 space-y-2">
     <li className="hover:bg-gray-700 px-4 py-2 rounded w-full text-left">Dashboard</li>
     <li className="hover:bg-gray-700 px-4 py-2 rounded w-full text-left">Profile</li>
     <li className="hover:bg-gray-700 px-4 py-2 rounded w-full text-left">Setting</li>
